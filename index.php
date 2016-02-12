a9389d16623781b37719008b89fafb0d1cd3c7a5
bl4cky <bl4cky1989@gmail.com>
2016-02-12 22:50:46 +0100

    carousel begun

--- index.php
+++ index.php
@@ -23,6 +23,63 @@
 $currentPage="aktionen";
 include('./html/navi.php')
 ?>
+<div>
+
+
+
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+
+            <div class="fill" style="background-image: url('/Computerspital/images/ihr-dialog-telekom-vertriebspartner-1000-400.jpg');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 1</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 2</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 3</h2>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+</header>
+</div>
+
+<!-- Header Carousel -->
+
+
+
+
+
+
+
+
+
+
 
 <!-- AKTIONEN ANFANG -->
 <div class="container container-fluid" >
